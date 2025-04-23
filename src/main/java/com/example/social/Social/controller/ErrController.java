f8dac888384602818d5740ad38e94818c3b31cc6
best-rion <husayn.buet@gmail.com>
2025-04-24 01:57:37 +0600

    Added error handling

--- src/main/java/com/example/social/Social/controller/ErrController.java
+++ src/main/java/com/example/social/Social/controller/ErrController.java
@@ -1,12 +1,13 @@
 package com.example.social.Social.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController
+public class ErrController implements ErrorController
 {
-	@GetMapping("/error")
+	@GetMapping ("/error")
 	public String handleError()
 	{
 		return "error/error";
